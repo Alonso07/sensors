057561ec92827de84a096b12da9f06a3ae029773
Alonso07 <alonsoram07@gmail.com>
2015-10-28 16:54:15 -0600

    Create funciones_sensores.h
    funciones para realizar diferentes operaciones con los sensores.

---
+++ funciones_sensores.h
@@ -0,0 +1,10 @@
+void leerSensor(unsigned short initial_direction, unsigned short sensor_direction);
+void initial_sensors();
+void calibrarG();
+void leer_MPU();
+void leer_COMPAS();
+void datos_MPU();
+void datos_COMPAS();
+
+void enviar_datos_mpu();
+void enviar_datos_compas();
